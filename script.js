f1ae8b949aa7e23fcc59eb7ac7088841f815d15b
Guilherme Alves <guilhermealves38@hotmail.com>
2021-07-04 06:46:19 -0300

    UpdateJS+CSS

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 function Verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.querySelector('input#txtano')
-    var res = document.querySelector('div#res')
+    let data = new Date()
+    let ano = data.getFullYear()
+    let fano = document.querySelector('input#txtano')
+    let res = document.querySelector('div#res')
 
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert(`[ERRO 404] Verifique os Dados e Tente Novamente`)
     }
     else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-        var genero = ''
-        var img = document.createElement('img')
+        let fsex = document.getElementsByName('radsex')
+        let idade = ano - Number(fano.value)
+        let img = document.createElement('img')
         img.setAttribute('id','foto')
 
         if(fsex[0].checked) {
-           genero = 'Homem'
+           let Homen = 'Homem'
 
             if (idade >= 0 && idade < 10) {
                 img.setAttribute('src','image/kidsman.png')
@@ -36,9 +35,11 @@ function Verificar() {
             else {
                 img.setAttribute('src','image/oldman.png')
             }
+
+            res.innerHTML = `Detectamos <strong style='color:Navy;font-weight: bold';>${Homen}</strong> com <strong style='font-weight: bold;font-size: 18px';>${idade}</strong> Anos. <br> <br> <br>`
         }
         else  {
-            genero = 'Mulher'
+            let Mulher = 'Mulher'
 
             if (idade > 0 && idade < 10) {
                 img.setAttribute('src','image/kidsfemale.png')
@@ -59,9 +60,9 @@ function Verificar() {
             else {
                 img.setAttribute('src','image/oldwo.png')
             }
+            res.innerHTML = `Detectamos <strong style='color:DarkMagenta;font-weight: bold';>${Mulher}</strong> com <strong style='font-weight: bold;font-size: 18px';>${idade}</strong> Anos. <br> <br> <br> `
         }
-    
-        res.innerHTML = `Detectamos ${genero} com ${idade} Anos. <br> <br> <br>`  
+
         res.appendChild(img)
     }
     
